54b6619e00e730b25c771dcd7f4c1112d686d70d
bl <bouchkhaf@gmail.com>
2025-11-03 11:17:43 +0100

    first bean

--- src/main/java/fr/devavance/metier/beans/User.java
+++ src/main/java/fr/devavance/metier/beans/User.java
@@ -11,6 +11,34 @@ public class User {
     private String password = "";  // Mot de passe
     private String profil = "";  // Profil de l'utilisateur "admin" ou "user"
     private Boolean auth = false; // Important : déconnecté par défaut
+    
+    public User(){ }
+    
+    public String getUserName(){
+        return userName;
+    }
+    public String getPassword(){
+        return password;
+    }
+    public String getProfil(){
+        return profil;
+    }
+    public Boolean getAuth(){
+        return auth;
+    }
+    
+    public void setUserName(String userName){
+        this.userName = userName;
+    }
+    public void setPassword(String password){
+        this.password = password;
+    }
+    public void setProfil(String profil){
+        this.profil = profil;
+    }
+    public void setAuth(Boolean auth){
+        this.auth = auth;
+    }
 
     
 
